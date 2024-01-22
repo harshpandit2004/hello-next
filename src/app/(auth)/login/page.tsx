a0716b5d091bb2d0583465103ae1192441ef8e87
harsh@technokart.in <harsh@technokart.in>
2024-01-22 17:30:41 +0530

    added linking

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <div>Login Page</div>
+      <div>
+        new user? try <Link href={"./registration"}>registering</Link>
+      </div>
+    </div>
+  );
 }
--- src/app/(auth)/registration/page.tsx
+++ src/app/(auth)/registration/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 export default function RegistrationPage() {
-  return <div>Registration Page</div>;
+  return (
+    <div>
+      <div>Registration Page</div>
+      <div>
+        alredy registered? try <Link href={"./login"}>logging in</Link>
+      </div>
+    </div>
+  );
 }
