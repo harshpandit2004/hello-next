834b3b8a36e6191482e463e8ba18ce5a27b35f6f
harsh@technokart.in <harsh@technokart.in>
2024-01-22 17:04:45 +0530

    tried metadata :D

---
+++ src/app/company/export-system/sales-order/[specificSalesOrderId]/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    specificSalesOrderId: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title: string = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Product ${params.specificSalesOrderId}`);
+    }, 100);
+  });
+  return {
+    title,
+  };
+};
+
+export default function SpecificSalesOrder({ params }: Props) {
+  return <div>Details About: Product {params.specificSalesOrderId}</div>;
+}
